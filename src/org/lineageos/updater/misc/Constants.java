71f79e9c3eecfdd3e37698c2ef1315ad492808e7
Rajkale99 <kaleraj.rk@gmail.com>
2019-01-04 13:46:12 +0100

    rebrand colt

--- src/org/lineageos/updater/misc/Constants.java
+++ src/org/lineageos/updater/misc/Constants.java
@@ -1,4 +1,4 @@
-/*
+ /*
  * Copyright (C) 2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -35,11 +35,11 @@ public final class Constants {
     public static final String PROP_AB_DEVICE = "ro.build.ab_update";
     public static final String PROP_BUILD_DATE = "ro.build.date.utc";
     public static final String PROP_BUILD_VERSION = "ro.modversion";
-    public static final String PROP_DEVICE = "ro.pixys.device";
+    public static final String PROP_DEVICE = "ro.coltos.device";
     public static final String PROP_NEXT_DEVICE = "ro.updater.next_device";
-    public static final String PROP_RELEASE_TYPE = "ro.pixys.releasetype";
+    public static final String PROP_RELEASE_TYPE = "ro.coltos.releasetype";
     public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "lineage.updater.allow_downgrading";
-    public static final String PROP_UPDATER_URI = "pixys.updater.uri";
+    public static final String PROP_UPDATER_URI = "coltos.updater.uri";
 
     public static final String PREF_INSTALL_OLD_TIMESTAMP = "install_old_timestamp";
     public static final String PREF_INSTALL_NEW_TIMESTAMP = "install_new_timestamp";
